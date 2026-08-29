5ed3722d5e66c154e167bb3db3837f60b7be2e22
Vlad Zhovtani <zovtanyy@gmail.com>
2026-08-29 20:41:11 +0300

    fix(build): exclude TemplateUIready from tsconfig and eslint checks

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ const compat = new FlatCompat({
 
 export default tseslint.config(
   {
-    ignores: [".next"],
+    ignores: [".next", "TemplateUIready", "generated"],
   },
   ...compat.extends("next/core-web-vitals"),
   {
